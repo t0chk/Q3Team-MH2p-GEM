ac1a1ad08c11f7ded363c9d60cc7e8288a1d22d7
t0chk <86585817+t0chk@users.noreply.github.com>
2025-01-29 16:48:59 +0300

    1.1.1
    - enableGEM - исправлена структура обновления

--- Mods/enableGEM/install.sh
+++ Mods/enableGEM/install.sh
@@ -1,12 +1,40 @@
 #!/bin/ksh
 # Copyright (c) 2025 https://t.me/tochk
 
+if [[ -e /fs/sda0 ]]; then
+	srcPath=/fs/sda0
+	echo "Use SD1 card as source"
+elif [[ -e /fs/sdb0 ]]; then
+	mount -uw /fs/sdb0
+	srcPath=/fs/sdb0
+	echo "Use SD2 card as source"
+elif [[ -e /fs/usb0_0 ]]; then
+	mount -uw /fs/usb0_0
+	srcPath=/fs/usb0_0
+	echo "Use USB1 drive as source"
+else
+	echo "Error: cannot find any SD card/USB drive!"
+	exit 1
+fi
+
 [[ ! -e "/mnt/app" ]] && mount -t qnx6 /dev/mnanda0t177.1 /mnt/app
-echo "Mounting /mnt/app in r/w mode..."
+echo "Mounting /mnt/app as dest..."
 mount -uw /mnt/app
 
-/mnt/app/armle/usr/bin/pc b:0x5F22:0x243F:0.7 1 
+if [[ ! -e "/mnt/app/eso/bin/servicemgrmibhigh0" ]]; then
+    cp /mnt/app/eso/bin/servicemgrmibhigh /mnt/app/eso/bin/servicemgrmibhigh0
+    cp -f $srcPath/Storage/01/servicemgrmibhigh /mnt/app/eso/bin/
+    chmod 755 /mnt/app/eso/bin/servicemgrmibhigh
+    echo "enable GEM [OK]"
+fi
 
-echo "enable GEM [OK]"
+echo "Unmounting /mnt/app..."
 sync
 [[ -e "/mnt/app" ]] && umount -f /mnt/app
+
+[[ ! -e "/mnt/ota" ]] &&  mount -t qnx6 /dev/mnanda0t177.16 /mnt/ota
+echo "Mounting /mnt/ota as dest..."
+mount -uw /mnt/ota
+
+cp -f $srcPath/Storage/01/challenge.pub /mnt/ota/
+cp -f $srcPath/Storage/01/doas.conf /mnt/ota/
