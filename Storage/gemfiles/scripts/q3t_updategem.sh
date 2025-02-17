f46c47a126147458c26d6f6b05b98522cf3196e1
t0chk <86585817+t0chk@users.noreply.github.com>
2025-02-17 18:35:43 +0300

    3.1.1
    - обновлен q3t_updategem.sh

--- Storage/gemfiles/scripts/q3t_updategem.sh
+++ Storage/gemfiles/scripts/q3t_updategem.sh
@@ -1,3 +1,34 @@
 #!/bin/ksh
 
 echo "Starting of [q3t gem update] script..."
+
+if [[ -e /fs/sda0 ]]; then
+	modPath=/fs/sda0
+	echo "Use SD1 card as source"
+elif [[ -e /fs/sdb0 ]]; then
+	mount -uw /fs/sdb0
+	modPath=/fs/sdb0
+	echo "Use SD2 card as source"
+elif [[ -e /fs/usb0_0 ]]; then
+	mount -uw /fs/usb0_0
+	modPath=/fs/usb0_0
+	echo "Use USB1 drive as source"
+else
+	echo "[ERR] cannot find any SD card/USB drive!"
+	exit 1
+fi
+
+# Монтирование точек
+[[ ! -e "/mnt/app" ]] && mount -t qnx6 /dev/mnanda0t177.1 /mnt/app
+echo "Mounting /mnt/app as dest..."
+mount -uw /mnt/app
+
+if [[ ! -e "$modPath/Storage/enablegem/doas.conf" ]]; then
+	echo "[ERR] doas.conf not found in [$modPath]/Storage/enablegem"
+	exit 1
+fi
+
+cp -fv $modPath/Storage/enablegem/doas.conf /mnt/ota/
+
+cp -fV $modPath/Storage/gemfiles /mnt/app/eso/hmi/engdefs
+chmod 755 /mnt/app/eso/hmi/engdefs/scripts/*
