2cafa2172d55e6bdecd465148a3bacfa0a2e4673
t0chk <86585817+t0chk@users.noreply.github.com>
2025-03-04 14:37:29 +0300

    3.1.5

--- Storage/gemfiles/scripts/iamsoxorry.sh
+++ Storage/gemfiles/scripts/iamsoxorry.sh
@@ -1,6 +1,7 @@
 #!/bin/ksh
 # Copyright (c) 2025 https://t.me/tochk https://t.me/q3f3chat
 
+# This tool takes the hex value stored in the control module and XORs it with 0xC9D2. 
 # echo $((16#C9D2))
 # 51666
 
