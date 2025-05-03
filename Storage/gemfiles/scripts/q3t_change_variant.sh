dd29923429c37e2a3b75a48d29755a822e2cb238
Aleksandr Smirnov <avsmirnov567@gmail.com>
2025-05-03 20:11:11 +0500

    Remove extra logic

--- Storage/gemfiles/scripts/q3t_change_variant.sh
+++ Storage/gemfiles/scripts/q3t_change_variant.sh
@@ -3,26 +3,9 @@
 
 echo "Starting of [change_variant] script..."
 
-if [[ -e /fs/sda0 ]]; then
-	mount -uw /fs/sda0
-	MODPATH=/fs/sda0
-	echo "Mounted SD1 card in r/w mode"
-elif [[ -e /fs/sdb0 ]]; then
-	mount -uw /fs/sdb0
-	MODPATH=/fs/sdb0
-	echo "Mounted SD2 card in r/w mode"
-elif [[ -e /fs/usb0_0 ]]; then
-	mount -uw /fs/usb0_0
-	MODPATH=/fs/usb0_0
-	echo "Mounted USB1 drive in r/w mode"
-else
-	echo "Error: cannot find any SD card/USB drive!"
-	exit 1
-fi
-
+echo "Mounting /mnt/app in r/w mode..."
 [[ ! -e "/mnt/app" ]] && mount -t qnx6 /dev/mnanda0t177.1 /mnt/app
 
-echo "Mounting /mnt/app in r/w mode..."
 echo "Variant before change: $(/mnt/app/armle/usr/bin/pc s:678364556:12 2>/dev/null)"
 
 /mnt/app/armle/usr/bin/pc s:678364556:12 M2P-HM-NDL-EU-AU-MQB-AL
--- Storage/gemfiles/scripts/q3t_check_coding.sh
+++ Storage/gemfiles/scripts/q3t_check_coding.sh
@@ -3,28 +3,8 @@
 
 echo "Starting of [check_coding] script..."
 
-if [[ -e /fs/sda0 ]]; then
-	mount -uw /fs/sda0
-	MODPATH=/fs/sda0
-	echo "Mounted SD1 card in r/w mode"
-elif [[ -e /fs/sdb0 ]]; then
-	mount -uw /fs/sdb0
-	MODPATH=/fs/sdb0
-	echo "Mounted SD2 card in r/w mode"
-elif [[ -e /fs/usb0_0 ]]; then
-	mount -uw /fs/usb0_0
-	MODPATH=/fs/usb0_0
-	echo "Mounted USB1 drive in r/w mode"
-else
-	echo "Error: cannot find any SD card/USB drive!"
-	exit 1
-fi
-
-[[ ! -e "/mnt/app" ]] && mount -t qnx6 /dev/mnanda0t177.1 /mnt/app
 echo "Mounting /mnt/app in r/w mode..."
-mount -uw /mnt/app
-
-[[ ! -d /mnt/app ]] && mount -t qnx6 /dev/mnanda0t177.1 /mnt/app
+[[ ! -e "/mnt/app" ]] && mount -t qnx6 /dev/mnanda0t177.1 /mnt/app
 
 echo "Variant: $(/mnt/app/armle/usr/bin/pc s:678364556:12 2>/dev/null)"
 echo "SW: $(/mnt/app/armle/usr/bin/pc b:0x5F22:0xF187 | awk '{print $13}' 2>/dev/null) HW: $(/mnt/app/armle/usr/bin/pc b:0x5F22:0xF191 | awk '{print $13}' 2>/dev/null)"
